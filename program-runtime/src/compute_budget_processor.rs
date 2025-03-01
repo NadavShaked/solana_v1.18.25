2ef1bec228279a39c68883facc81a24ca288de9f
Nadav Shaked <nadav2sh@gmail.com>
2025-03-01 23:12:04 +0200

    Increase MAX_COMPUTE_UNIT_LIMIT to maximum

--- program-runtime/src/compute_budget_processor.rs
+++ program-runtime/src/compute_budget_processor.rs
@@ -16,7 +16,7 @@ use {
 
 const MAX_HEAP_FRAME_BYTES: u32 = 256 * 1024;
 pub const DEFAULT_INSTRUCTION_COMPUTE_UNIT_LIMIT: u32 = 200_000;
-pub const MAX_COMPUTE_UNIT_LIMIT: u32 = 1_400_000;
+pub const MAX_COMPUTE_UNIT_LIMIT: u32 = u32::MAX;
 
 /// The total accounts data a transaction can load is limited to 64MiB to not break
 /// anyone in Mainnet-beta today. It can be set by set_loaded_accounts_data_size_limit instruction
